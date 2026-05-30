c91210bf941ab5d8c4c3e4a144a6fd60cb87fd48
Lucas Vorique Vieira de Melo <vorique.ads@gmail.com>
2026-05-30 18:42:57 -0300

    feat: Atualize as pastas do projeto para melhor estruturação.

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Bem Vindo ao DocFlow, seu aplicativo de agendamento de consultas médicas!</h1>
+    </main>
+  );
+}
